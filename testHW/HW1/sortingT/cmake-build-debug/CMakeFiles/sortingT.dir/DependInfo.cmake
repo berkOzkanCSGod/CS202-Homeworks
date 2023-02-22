9b81e8c1d017bff805c9a79af2963b7f79bf847d
Berk Özkan <82842011+berkOzkanCSGod@users.noreply.github.com>
2023-02-22 13:16:34 +0300

    Add files via upload
    HW1

---
+++ testHW/HW1/sortingT/cmake-build-debug/CMakeFiles/sortingT.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/berkozkan/Library/Mobile Documents/com~apple~CloudDocs/C++_projects/CS202/2023/CS202/testHW/HW1/sortingT/main.cpp" "/Users/berkozkan/Library/Mobile Documents/com~apple~CloudDocs/C++_projects/CS202/2023/CS202/testHW/HW1/sortingT/cmake-build-debug/CMakeFiles/sortingT.dir/main.cpp.o"
+  "/Users/berkozkan/Library/Mobile Documents/com~apple~CloudDocs/C++_projects/CS202/2023/CS202/testHW/HW1/sortingT/sorting.cpp" "/Users/berkozkan/Library/Mobile Documents/com~apple~CloudDocs/C++_projects/CS202/2023/CS202/testHW/HW1/sortingT/cmake-build-debug/CMakeFiles/sortingT.dir/sorting.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ testHW/HW1/sortingT/cmake-build-debug/CMakeFiles/sortingT.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sortingT.dir/main.cpp.o"
+  "CMakeFiles/sortingT.dir/sorting.cpp.o"
+  "sortingT"
+  "sortingT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sortingT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
